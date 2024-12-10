cb03ba83f6f3791fd2894de11c8a0deacc575b2d
joaoLouceiro <jrlouceiro@live.com>
2024-12-10 20:51:35 +0000

    Exercise 1.4 - celcius to fahrenheit

---
+++ 1-4.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main(void) {
+  int low, high;
+  float celc, fahr, step;
+  low = 0;
+  high = 50;
+  step = 5;
+  celc = low;
+
+  while (celc < high) {
+    fahr = celc * (9.0 / 5.0) + 32.0;
+    printf("%4.0f %6.1f\n", celc, fahr);
+    celc += step;
+  }
+  return 0;
+}
